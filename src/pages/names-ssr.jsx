6241e07234bd6d3c2c2235c78b4799c089e60c01
Prashant <chauhanprashant480@gmail.com>
2024-12-13 21:55:37 +0530

    ssr component with props example added

---
+++ src/pages/names-ssr.jsx
@@ -0,0 +1,33 @@
+import { fetchNames } from "@/utils/fetch-names";
+
+const NamesSSR = (props) => {
+    const output = props.names.map((item, idx) => {
+        return (
+            <li key={`name-${idx}`}>
+                {item.id} : {item.name}
+            </li>
+        )
+    })
+
+    return (
+        <ul>
+           {output} 
+        </ul>
+    )
+}
+
+export const getServerSideProps = async (context) => {
+    let names = []
+    try {
+        names = await fetchNames()
+    } catch(err) {
+        console.error("fetch names error", err)
+    }
+    return {
+        props: {
+            names
+        }
+    }
+}
+
+export default NamesSSR
