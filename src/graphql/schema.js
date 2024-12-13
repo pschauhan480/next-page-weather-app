75c0a0dbfc453e52faf85687f78323bae50afdee
Prashant <chauhanprashant480@gmail.com>
2024-12-13 23:05:56 +0530

    graphql endpoint enabled

--- src/graphql/schema.js
+++ src/graphql/schema.js
@@ -1,1 +1,20 @@
 import gql from "graphql-tag";
+
+export const typeDefs = gql`
+    type FriendsType {
+        zip: String!
+        weather: String!
+    }
+
+    type LocationWeatherType {
+        zip: String!
+        weather: String!
+        tempC: String!
+        tempF: String!
+        friends: [FriendsType]!
+    }
+
+    type Query {
+        weather(zip: String): [LocationWeatherType]!
+    }
+`;
